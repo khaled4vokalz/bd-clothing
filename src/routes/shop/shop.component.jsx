e3ef7a05edfcaf08958b4833f0543ecb85b0d1ee
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2023-08-26 16:39:34 +0600

    Shop component and product card component
    - use ProductsContext to handle the product data for the components
    - currently uses Static data, in future we'll introduce firebase in this mix

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react"
+import { ProductsContext } from "../../context/products.context"
+import ProductCard from "../../components/product-card/product-card.component";
+
+import './shop.styles.scss';
+
+export default function Shop() {
+  const { products } = useContext(ProductsContext);
+  return (
+    <div className="products-container">
+      {
+        products.map(product => {
+          return (
+            <ProductCard key={product.id} product={product} />
+          )
+        })
+      }
+    </div>
+  )
+}
