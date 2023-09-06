a05551f131fb1903740fc024cbb25f58e33190ec
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2023-09-06 16:56:25 +0600

    navigate to correct route on clicking category from home

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import {
   CategoryItemContainer,
   BackgroundImage,
   CategoryBodyContainer,
 } from './category-item.styles';
 
-export default function CategoryItem({ category: { imageUrl, title } }) {
+export default function CategoryItem({ category: { imageUrl, title, route } }) {
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    navigate(route);
+  };
   return (
-    <CategoryItemContainer>
+    <CategoryItemContainer onClick={onClickHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <CategoryBodyContainer>
         <h2> {title}</h2>
