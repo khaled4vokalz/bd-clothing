621fa6faaa2a773a3a464d914f4342361d9d7744
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2023-09-09 19:39:39 +0600

    Enter Redux-Persist: keep the state of the store persisted in the local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,22 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import { rootReducer } from './root.reducer';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger];
 
 const composedEnhancer = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, composedEnhancer);
+export const store = createStore(persistedReducer, composedEnhancer);
+
+export const persistor = persistStore(store);
