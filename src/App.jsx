216c642f981bed69e63a0ed311a7f381f4056e91
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2023-09-18 22:39:50 +0600

    Redux-Saga: checkUserSession

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { getCurrentUser } from './utils/firebase/firebase.utils';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { checkUserSession } from './store/user/user.action';
 
 export default function App() {
+  const dispatch = useDispatch();
   useEffect(() => {
-    getCurrentUser().then(console.log);
+    dispatch(checkUserSession());
   }, []);
 
   return (
